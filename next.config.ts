54440436541915f45bfcad451cdf1a7c65300777
Jack Duong <duonganhtuan04@gmail.com>
2026-02-01 09:03:11 +1300

    feat: configure Next.js for static export and add GitHub Actions workflow for Pages deployment.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const isProd = true;
+const repo = 'elysion-next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
